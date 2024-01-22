c0616d9f2f556b58a1e200686394e78be037b638
Matthew Slauson <matthew@slauson.io>
2024-01-22 17:10:23 -0600

    add example starter files

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,43 @@
-use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
-use crossterm::terminal::{
-    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+use std::io::{self, stdout};
+
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
 };
-use ratatui::backend::CrosstermBackend;
-use ratatui::layout::{Constraint, Direction, Layout};
-use ratatui::Terminal;
-use std::io;
-use tui_textarea::{Input, Key, TextArea};
-
-use crate::ui::textarea;
-
-mod ui;
+use ratatui::{prelude::*, widgets::*};
 
 fn main() -> io::Result<()> {
-    let stdout = io::stdout();
-    let mut stdout = stdout.lock();
     enable_raw_mode()?;
-    crossterm::execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut term = Terminal::new(backend)?;
+    stdout().execute(EnterAlternateScreen)?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
 
-    let mut textarea = [TextArea::default(), TextArea::default()];
-
-    let layout = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref());
-
-    let mut which = 0;
-    textarea::activate(&mut textarea[0]);
-    textarea::inactivate(&mut textarea[1]);
-
-    loop {
-        term.draw(|f| {
-            let chunks = layout.split(f.size());
-            for (textarea, chunk) in textarea.iter().zip(chunks.iter()) {
-                let widget = textarea.widget();
-                f.render_widget(widget, *chunk);
-            }
-        })?;
-        match crossterm::event::read()?.into() {
-            Input { key: Key::Esc, .. } => break,
-            Input {
-                key: Key::Char('x'),
-                ctrl: true,
-                ..
-            } => {
-                textarea::inactivate(&mut textarea[which]);
-                which = (which + 1) % 2;
-                textarea::activate(&mut textarea[which]);
-            }
-            input => {
-                textarea[which].input(input);
-            }
-        }
+    let mut should_quit = false;
+    while !should_quit {
+        terminal.draw(ui)?;
+        should_quit = handle_events()?;
     }
 
     disable_raw_mode()?;
-    crossterm::execute!(
-        term.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    term.show_cursor()?;
-
-    println!("Left textarea: {:?}", textarea[0].lines());
-    println!("Right textarea: {:?}", textarea[1].lines());
+    stdout().execute(LeaveAlternateScreen)?;
     Ok(())
+}
+
+fn handle_events() -> io::Result<bool> {
+    if event::poll(std::time::Duration::from_millis(50))? {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(true);
+            }
+        }
+    }
+    Ok(false)
+}
+
+fn ui(frame: &mut Frame) {
+    frame.render_widget(
+        Paragraph::new("Hello World!")
+            .block(Block::default().title("Greeting").borders(Borders::ALL)),
+        frame.size(),
+    );
 }
